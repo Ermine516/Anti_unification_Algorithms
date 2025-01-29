3ab543c793d990cbc4587e98adc5d36027915e0f
Ermine516 <cernadavid1@gmail.com>
2025-01-29 11:49:33 +0100

    Remaking the repository

--- AUExceptions/InvalidAppException.java
+++
@@ -1,9 +0,0 @@
-package AUExceptions;
-public class InvalidAppException extends Exception { 
-    public InvalidAppException() {
-        super();
-    }
-    public InvalidAppException(String errorMessage) {
-        super(errorMessage);
-    }
-}
